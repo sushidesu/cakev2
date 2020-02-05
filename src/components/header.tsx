e0d5eefdaa76833ea887d9b69b12f3dd308a6727
sushi-desu <hugemerge@gmail.com>
2020-02-05 14:19:00 +0900

    Add header

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Navbar, Container } from "react-bulma-components"
+
+export default () => (
+  <Navbar>
+    <Container>
+      <Navbar.Brand>
+        <Navbar.Item>Cake</Navbar.Item>
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+)
--- src/options.tsx
+++ src/options.tsx
@@ -1,11 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import styled from "styled-components"
+import Header from "./components/header"
 
+const Wrapper = styled.div`
+  margin: 10px;
+  .sidemenu {
+    min-width: 230px;
+  }
+`
 
 const Options = () => (
-  <>
+  <Wrapper>
+    <Header />
     <h1>Cake Option</h1>
-  </>
+  </Wrapper>
 )
 
 ReactDOM.render(<Options />, document.getElementById('root'))
