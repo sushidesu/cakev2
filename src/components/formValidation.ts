3034316de24fb62c7cad1b492464b993894f9105
sushi-desu <hugemerge@gmail.com>
2020-02-12 15:34:40 +0900

    Add jancode validation

--- src/components/formValidation.ts
+++ src/components/formValidation.ts
@@ -1,10 +1,37 @@
 import { IShopItem } from "../shopItem"
 
 export const formIsInvalid = (formValue: IShopItem) => {
+  const { name, price, weight, stockRakuten, stockMakeshop, jancode } = formValue
+
   // required fields
-  const { name, price, weight, stockRakuten, stockMakeshop } = formValue
   const required = [name, price, weight, stockRakuten, stockMakeshop]
     .every(field => field !== "")
 
-  return ![required].every(f => f)
+  // jancode
+  const jan = jancode === '' || isJancode(jancode)
+
+  return ![required, jan].every(f => f)
+}
+
+const isJancode = (jan: string): boolean => {
+  if (jan.length !== 13) {
+    return false
+  }
+
+  const digits = jan.split('').map(n => parseInt(n))
+  if (digits.some(n => isNaN(n))) {
+    return false
+  }
+
+  const odd = digits.filter((_, i) => i % 2 === 1)
+  const even = digits.filter((_, i) => i !== 12 && i % 2 === 0)
+
+  let total = 0
+  for (let i = 0; i < 6; i++) {
+    total += even[i] + odd[i] * 3
+  }
+
+  const res = total % 10
+  const checkd = digits[12]
+  return res === 0 ? 0 === checkd : 10 - res === checkd
 }
