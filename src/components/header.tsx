fbcb870205c658c117dfd680487db391e189eaec
sushi-desu <hugemerge@gmail.com>
2020-02-19 14:19:03 +0900

    Add dropdown menu to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,27 @@
 import React from "react"
 import { Navbar, Container } from "react-bulma-components"
+import Logo from "../images/sweets_cheesecake.png"
 
 export default () => (
-  <Navbar>
+  <Navbar className={"has-shadow"}>
     <Container>
       <Navbar.Brand>
-        <Navbar.Item>Cake</Navbar.Item>
+        <div className="navbar-item" style={{ userSelect: "none" }}>
+          <img src={Logo} />
+          <span style={{ marginLeft: "6px", fontSize: "1.1em" }}>Cake</span>
+        </div>
       </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Container position="end">
+          <Navbar.Item dropdown hoverable>
+            <Navbar.Link>設定</Navbar.Link>
+            <Navbar.Dropdown>
+              <Navbar.Item>インポート</Navbar.Item>
+              <Navbar.Item>エクスポート</Navbar.Item>
+            </Navbar.Dropdown>
+          </Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
     </Container>
   </Navbar>
 )
