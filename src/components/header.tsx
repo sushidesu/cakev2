d47fe68ac8c45536c295ab89def0515417113f86
sushi-desu <hugemerge@gmail.com>
2020-02-19 16:10:24 +0900

    Add modal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,51 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Navbar, Container } from "react-bulma-components"
 import Logo from "../images/sweets_cheesecake.png"
+import ImportModal from "./importModal"
 
-export default () => (
-  <Navbar className={"has-shadow"}>
-    <Container>
-      <Navbar.Brand>
-        <div className="navbar-item" style={{ userSelect: "none" }}>
-          <img src={Logo} />
-          <span style={{ marginLeft: "6px", fontSize: "1.1em" }}>Cake</span>
-        </div>
-      </Navbar.Brand>
-      <Navbar.Menu>
-        <Navbar.Container position="end">
-          <Navbar.Item dropdown hoverable>
-            <Navbar.Link>設定</Navbar.Link>
-            <Navbar.Dropdown>
-              <Navbar.Item>インポート</Navbar.Item>
-              <Navbar.Item>エクスポート</Navbar.Item>
-            </Navbar.Dropdown>
-          </Navbar.Item>
-        </Navbar.Container>
-      </Navbar.Menu>
-    </Container>
-  </Navbar>
-)
+export default () => {
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    if (show) {
+      document.documentElement.classList.add("is-clipped")
+    } else {
+      document.documentElement.classList.remove("is-clipped")
+    }
+  }, [show])
+
+  return (
+    <>
+      <Navbar className={"has-shadow"}>
+        <Container>
+          <Navbar.Brand>
+            <div className="navbar-item" style={{ userSelect: "none" }}>
+              <img src={Logo} />
+              <span style={{ marginLeft: "6px", fontSize: "1.1em" }}>Cake</span>
+            </div>
+          </Navbar.Brand>
+          <Navbar.Menu>
+            <Navbar.Container position="end">
+              <Navbar.Item dropdown hoverable>
+                <Navbar.Link>設定</Navbar.Link>
+                <Navbar.Dropdown>
+                  <Navbar.Item onClick={() => setShow(true)}>
+                    インポート
+                  </Navbar.Item>
+                  <Navbar.Item
+                    onClick={() => {
+                      window.alert("export")
+                    }}
+                  >
+                    エクスポート
+                  </Navbar.Item>
+                </Navbar.Dropdown>
+              </Navbar.Item>
+            </Navbar.Container>
+          </Navbar.Menu>
+        </Container>
+      </Navbar>
+
+      <ImportModal show={show} setShow={setShow} />
+    </>
+  )
+}
