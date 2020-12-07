c272f591bed4ee1aa7dd764d7f6aebb5d1cb68a5
sushidesu <hugemerge@gmail.com>
2020-12-07 11:43:09 +0900

    remove webpack-extension-reloader

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,13 @@
-const merge = require('webpack-merge')
-const path = require('path');
-const base = require('./webpack.config.js');
-const ExtensionReloader = require('webpack-extension-reloader');
+const merge = require("webpack-merge")
+const path = require("path")
+const base = require("./webpack.config.js")
 
 module.exports = merge(base, {
-  mode: 'development',
+  mode: "development",
   watch: true,
   entry: {
-    popup: path.resolve(__dirname, 'src/popup.tsx'),
-    background: path.resolve(__dirname, 'src/background.ts'),
-    options: path.resolve(__dirname, 'src/options.tsx')
+    popup: path.resolve(__dirname, "src/popup.tsx"),
+    background: path.resolve(__dirname, "src/background.ts"),
+    options: path.resolve(__dirname, "src/options.tsx"),
   },
-  plugins: [
-    new ExtensionReloader({
-      manifest: path.resolve(__dirname, 'public/manifest.json'),
-      port: 3001,
-      reloadPage: true,
-      entries: {
-        background: 'background',
-        options: 'options',
-        extensionPage: 'popup'
-      }
-    })
-  ]
 })
