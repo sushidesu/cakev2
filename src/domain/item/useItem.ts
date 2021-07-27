8ebe95cd3458df9b47559d5d173b79c342c68440
sushidesu <hugemerge@gmail.com>
2021-07-27 17:32:29 +0900

    itemのリストを扱うhooks

---
+++ src/domain/item/useItem.ts
@@ -0,0 +1,87 @@
+import { useCallback, useEffect, useState } from "react"
+import { Item } from "./item"
+import { ItemId } from "./itemId"
+import { CustomBlock } from "../block/block"
+import { ChromeStorageClient } from "../../infra/chromeStorageClient"
+
+export interface ItemCollection {
+  itemList: Item[]
+  create: () => void
+  update: (props: ItemUpdateProps) => void
+  updateBlock: (props: BlockUpdateProps) => void
+  remove: (id: ItemId) => void
+  duplicate: (id: ItemId) => void
+  select: (id: ItemId) => void
+}
+
+export type ItemFormValue = {
+  name: string
+  price: string
+  weight: string
+  stockRakuten: string
+  stockMakeshop: string
+  jancode: string
+}
+
+export type ItemUpdateProps = {
+  id: ItemId
+  value: ItemFormValue
+}
+export type BlockUpdateProps = {
+  id: ItemId
+  value: CustomBlock[]
+}
+
+export const useItemCollection = (): ItemCollection => {
+  const storage = new ChromeStorageClient()
+  const [items, setItems] = useState<Item[]>([])
+
+  useEffect(() => {
+    let unmounted = false
+
+    const fetcher = async () => {
+      const result = await storage.getAllItems()
+      console.log({ result })
+      if (!unmounted) {
+        setItems(result)
+      }
+    }
+    fetcher()
+
+    return () => {
+      unmounted = true
+    }
+  }, [])
+
+  const create = useCallback(() => {
+    // TODO
+  }, [])
+  const update = useCallback(({}: ItemUpdateProps) => {
+    // TODO
+  }, [])
+  const updateBlock = useCallback(({}: BlockUpdateProps) => {
+    // TODO
+  }, [])
+  const remove = useCallback((id: ItemId) => {
+    console.log(id)
+    // TODO
+  }, [])
+  const duplicate = useCallback((id: ItemId) => {
+    console.log(id)
+    // TODO
+  }, [])
+  const select = useCallback((id: ItemId) => {
+    console.log(id)
+    // TODO
+  }, [])
+
+  return {
+    itemList: items,
+    create,
+    update,
+    updateBlock,
+    remove,
+    duplicate,
+    select,
+  }
+}
