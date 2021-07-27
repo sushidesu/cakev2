cdbe309cad14ffe917a8d4ed0cbbd33fdf9d657c
sushidesu <hugemerge@gmail.com>
2021-07-27 12:13:40 +0900

    remove

--- src/domain/block/block.ts
+++
@@ -1,8 +0,0 @@
-import { BlockId } from "./blockId"
-
-export type BlockTypes = "heading" | "text" | "image" | "table"
-
-export interface Block {
-  readonly id: BlockId
-  readonly type: BlockTypes
-}
