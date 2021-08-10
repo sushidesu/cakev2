e9625d6baac4aa999fb20ba51d16e6e5b9b4fb3b
sushidesu <hugemerge@gmail.com>
2021-08-10 18:38:41 +0900

    fix: ImageBlockEditorをいい感じに

--- src/components/editor/ImageBlockEditor.tsx
+++ src/components/editor/ImageBlockEditor.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import { ImageBlock } from "../../domain/customBlock/block"
 import { BlockEditorProps } from "./BlockEditor"
 import { BlockEditorWrapper } from "./BlockEditorWrapper"
@@ -11,29 +12,51 @@ export function ImageBlockEditor({
 }: BlockEditorProps<ImageBlock>): JSX.Element {
   return (
     <BlockEditorWrapper label={"画像"} {...rest}>
-      <Input
-        defaultValue={block.value.imageUrl}
-        onBlur={e => {
-          e.persist()
-          update({
-            id: block.id,
-            mutation: prev => {
-              if (prev.type === "image") {
-                return {
-                  ...prev,
-                  type: "image",
-                  value: {
-                    ...prev.value,
-                    imageUrl: e.target.value,
-                  },
-                }
-              } else {
-                return prev
-              }
-            },
-          })
-        }}
-      />
+      <div className="media">
+        <div className="media-left">
+          <Thumbnail src={block.value.imageUrl} />
+        </div>
+        <div className="media-content">
+          <Input
+            defaultValue={block.value.imageUrl}
+            onBlur={e => {
+              e.persist()
+              update({
+                id: block.id,
+                mutation: prev => {
+                  if (prev.type === "image") {
+                    return {
+                      ...prev,
+                      type: "image",
+                      value: {
+                        ...prev.value,
+                        imageUrl: e.target.value,
+                      },
+                    }
+                  } else {
+                    return prev
+                  }
+                },
+              })
+            }}
+          />
+        </div>
+      </div>
     </BlockEditorWrapper>
   )
 }
+
+const Figure = styled.figure`
+  img {
+    position: relative;
+    object-fit: contain;
+    width: 128px;
+    height: 128px;
+    background-color: #fff;
+    box-shadow: 2px 4px 6px rgba(200, 200, 200, 0.4);
+  }
+`
+
+const Thumbnail = ({ src }: { src: string }): JSX.Element => (
+  <Figure className={"image is-128x128"}>{src && <img src={src} />}</Figure>
+)
