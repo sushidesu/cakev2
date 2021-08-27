d50f41b4c5a4c9352733371aa4765a7ec5cd6b3a
sushidesu <hugemerge@gmail.com>
2021-08-27 17:30:27 +0900

    remove unused component

--- src/components/formSingleInputs.tsx
+++ src/components/formSingleInputs.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-import styled from "styled-components"
-import { Form, Media, InputProps } from "react-bulma-components"
+import { Form, InputProps } from "react-bulma-components"
 import { SingleItemFields } from "../shopItem"
 
 export type Props = {
@@ -33,37 +32,4 @@ export const FormInput = ({
     </Form.Control>
     <Form.Help color={"danger"}>{message}</Form.Help>
   </Form.Field>
-)
-
-const Figure = styled.figure`
-  img {
-    position: relative;
-    object-fit: contain;
-    width: 128px;
-    height: 128px;
-    background-color: #fff;
-  }
-`
-
-const Thumbnail: React.FC<{ src: string }> = ({ src }) => (
-  <Figure className={"image is-128x128"}>{src && <img src={src} />}</Figure>
-)
-
-export const FormImageInput = ({
-  label,
-  field,
-  value,
-  onChange,
-}: Props): JSX.Element => (
-  <Media>
-    <div className="media-left" style={{ backgroundColor: "#efefef" }}>
-      <Thumbnail src={value} />
-    </div>
-    <Media.Item>
-      <Form.Field>
-        <Form.Label>{label}</Form.Label>
-        <Form.Input name={field} value={value} onChange={onChange} />
-      </Form.Field>
-    </Media.Item>
-  </Media>
 )
