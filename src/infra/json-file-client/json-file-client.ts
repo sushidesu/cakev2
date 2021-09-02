ed92069ecb23073dd16442e5b3cc7ab37811d7fd
sushidesu <hugemerge@gmail.com>
2021-09-02 15:46:11 +0900

    fix: use unkown + User-Defined Type Guards instead of any

--- src/infra/json-file-client/json-file-client.ts
+++ src/infra/json-file-client/json-file-client.ts
@@ -16,36 +16,52 @@ export class JSONFileClient implements JsonClientInterface {
   /**
    * idは新規生成します
    */
-  public getItemsFromJSONFile(json: any): Item[] {
+  public getItemsFromJSONFile(json: unknown): Item[] {
     console.log("IMPORT JSON", { json })
 
     let items: Item[]
     // --- PARSE ---
-    if (hasOwn.call(json, "version") && hasOwn.call(json, "items")) {
-      // case V3
+    if (this.isCakeV3(json)) {
       console.log("import v3")
-      const storage = json as JSONScheme
       // convert V3 -> items
-      items = Object.values(storage.items)
+      items = Object.values(json.items)
         .sort((a, b) => a.order - b.order)
-        .map(itemJson => this.converter.JSON_V3ToEntity(itemJson))
-    } else if (hasOwn.call(json, "cakev2")) {
+        .map((itemJson) => this.converter.JSON_V3ToEntity(itemJson))
+    } else if (this.isCakeV2(json)) {
       // case V2
       console.log("import v2")
-      const storage_v2 = json as JSONScheme_V2
+      const storage_v2 = json
       // convert V2 -> V3 -> items
       const storage_v3 = this.scheme_v2_converter.convertStorageV2ToV3(
         storage_v2.cakev2
       )
       items = Object.values(storage_v3.items)
         .sort((a, b) => a.order - b.order)
-        .map(item => this.converter.JSON_V3ToEntity(item))
+        .map((item) => this.converter.JSON_V3ToEntity(item))
     } else {
       // case unknown
       throw Error("インポートに失敗しました。対応していないファイルです。")
     }
 
     return items
+  }
+
+  private isNotNullish(object: unknown): object is Record<string, unknown> {
+    return object != null
+  }
+  private isCakeV3(object: unknown): object is JSONScheme {
+    if (!this.isNotNullish(object)) {
+      return false
+    }
+
+    return hasOwn.call(object, "version") && hasOwn.call(object, "items")
+  }
+  private isCakeV2(object: unknown): object is JSONScheme_V2 {
+    if (!this.isNotNullish(object)) {
+      return false
+    }
+
+    return hasOwn.call(object, "cakev2")
   }
 
   public exportItemsAsJSONFile(items: Item[]): JSONScheme {
@@ -56,7 +72,7 @@ export class JSONFileClient implements JsonClientInterface {
       items: Object.fromEntries(
         items
           .map((item, index) => this.converter.entityToJSON(item, index))
-          .map(item => [item.id, item])
+          .map((item) => [item.id, item])
       ),
     }
   }
--- src/usecase/interface/json-client-interface.ts
+++ src/usecase/interface/json-client-interface.ts
@@ -2,6 +2,6 @@ import { Item } from "../../domain/item/item"
 import { JSONScheme } from "../../infra/json-file-client/interface/scheme"
 
 export interface JsonClientInterface {
-  getItemsFromJSONFile(json: any): Item[]
+  getItemsFromJSONFile(json: unknown): Item[]
   exportItemsAsJSONFile(items: Item[]): JSONScheme
 }
