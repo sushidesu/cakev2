0e5888ad01e3c89d325806d54146a39e5281ed35
sushidesu <hugemerge@gmail.com>
2021-09-06 10:53:27 +0900

    refactor: remake button without react-bulma-components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,39 @@
 import React from "react"
-import { Button as BulmaButton, ButtonProps } from "react-bulma-components"
+import clsx from "clsx"
+
+type Color =
+  | "white"
+  | "light"
+  | "dark"
+  | "black"
+  | "text"
+  | "ghost"
+  | "primary"
+  | "link"
+  | "info"
+  | "success"
+  | "warning"
+  | "danger"
 
 export type Props = {
-  visible?: boolean
-} & ButtonProps
+  visible?: boolean | undefined
+  color?: Color | undefined
+  outlined?: boolean | undefined
+  // primitive props
+  children?: React.ReactNode
+  onClick?: JSX.IntrinsicElements["button"]["onClick"]
+}
 
-export function Button({ visible = true, ...rest }: Props): JSX.Element | null {
-  return visible ? <BulmaButton {...rest} /> : null
+export function Button({
+  visible = true,
+  color,
+  outlined = false,
+  ...rest
+}: Props): JSX.Element | null {
+  return visible ? (
+    <button
+      className={clsx("button", `is-${color}`, outlined && "is-outlined")}
+      {...rest}
+    />
+  ) : null
 }
