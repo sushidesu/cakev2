caa98538933d8ba6e955c9da2ec515d33ff0a1f5
sushidesu <hugemerge@gmail.com>
2022-02-13 16:21:32 +0900

    test(JSON): json -> entityのときにblock,subBlockが正しく変換される

--- src/infra/json-file-client/tests/json-scheme-converter.test.ts
+++ src/infra/json-file-client/tests/json-scheme-converter.test.ts
@@ -1,6 +1,7 @@
 import { BlockId } from "../../../domain/block/blockId"
 import { Item } from "../../../domain/item/item"
 import { ItemId } from "../../../domain/item/itemId"
+import { ItemJSON } from "../interface/scheme"
 import { JSONShcemeConverter } from "../json-scheme-converter"
 
 describe(`JSONSchemeConverter`, () => {
@@ -68,6 +69,69 @@ describe(`JSONSchemeConverter`, () => {
         ],
         order: 0,
       })
+    })
+  })
+
+  describe(`JSON_V3ToEntity()`, () => {
+    it(`block,subBlockを正しく変換する`, () => {
+      const json: ItemJSON = {
+        id: "item-01",
+        name: "テスト商品",
+        price: 0,
+        weight: 0,
+        order: 0,
+        stockRakuten: 0,
+        stockMakeshop: 0,
+        jancodeString: "",
+        blocks: [
+          {
+            id: "block-01",
+            type: "text",
+            value: {
+              content: "hello",
+            },
+          },
+        ],
+        subBlocks: [
+          {
+            id: "sub-01",
+            type: "heading",
+            value: {
+              content: "HELLO!",
+            },
+          },
+        ],
+      }
+      const actual = converter.JSON_V3ToEntity(json)
+      const expected: Item = {
+        // IDは常に新規生成する
+        id: expect.any(ItemId),
+        name: "テスト商品",
+        price: 0,
+        weight: 0,
+        stockRakuten: 0,
+        stockMakeshop: 0,
+        jancode: undefined,
+        blocks: [
+          {
+            id: BlockId.reconstruct("block-01"),
+            type: "text",
+            value: {
+              content: "hello",
+            },
+          },
+        ],
+        subBlocks: [
+          {
+            id: BlockId.reconstruct("sub-01"),
+            type: "heading",
+            value: {
+              content: "HELLO!",
+            },
+          },
+        ],
+      }
+      expect(actual).toStrictEqual(expected)
     })
   })
 })
